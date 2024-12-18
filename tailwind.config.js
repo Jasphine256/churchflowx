bb1e49c84bed32c00115197cff6f139af48178bc
Jasphine2020 <jasphinesoftware@gmail.com>
2024-12-19 02:39:49 +0300

    another tailwind config  bug fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,15 @@
 import {nextui} from '@nextui-org/theme'
 
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
+
+  export const content = [
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
     './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'
-  ],
-  theme: {
+  ]
+  export const theme = {
     extend: {
     },
-  },
-  darkMode: "class",
-  plugins: [nextui()],
-}
+  }
+  export const darkMode = "class"
+  export const plugins = [nextui()]
