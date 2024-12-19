fa47d7dcdcbd4b62a38b3920170f27f76255d468
Jasphine2020 <jasphinesoftware@gmail.com>
2024-12-19 12:45:03 +0300

    removed feature details url
    the removed url is not required at the moment

--- components/widgets/Feature.jsx
+++ components/widgets/Feature.jsx
@@ -6,7 +6,6 @@ export default function Feature({name, subtitle, url, icon}) {
   return (
     <Card className="py-4">
       <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
-        <Link href={url} className="text-tiny uppercase font-bold">{"DETAILS >>>"}</Link>
         <small className="text-default-500">{subtitle}</small>
         <h4 className="font-bold text-large">{name}</h4>
       </CardHeader>
